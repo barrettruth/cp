37728780f92aa8a9c07a1fd1fcbbb3aacee06d25
Barrett Ruth <br@barrettruth.com>
2026-08-25 16:16:00 +1000

    feat(codeforces): round 1050 a and e

---
+++ codeforces/1050/e.py
@@ -0,0 +1,39 @@
+# {{{
+import sys
+from bisect import bisect_left, bisect_right, insort
+from collections import Counter, defaultdict, deque
+from functools import cache, cmp_to_key, reduce
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, combinations, permutations, product
+from math import comb, gcd, inf, isqrt, lcm, perm
+from string import ascii_lowercase, ascii_uppercase
+
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10**4)
+
+
+def db(*args):
+    print(*args, file=sys.stderr)
+
+
+# }}}
+
+
+def solve():
+    pass
+
+
+# {{{
+def main():
+    for _ in range(int(input())):
+        solve()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# }}}
+
+# vim: foldmethod=marker foldlevel=0
