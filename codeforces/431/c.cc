9ca99539bcdf65bb2a7ed45c2de7037973aee14c
Barrett Ruth <br@barrettruth.com>
2026-08-31 09:47:41 +1000

    refactor(codeforces): standardize contest 431

--- codeforces/431/c.cc
+++ codeforces/431/c.cc
@@ -1,11 +1,83 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> // {{{
+
+#if __cplusplus >= 202002L && defined(__cpp_lib_ranges)
+namespace rv = std::views;
+namespace rs = std::ranges;
+#endif
+
+template <typename T>
+using vec = std::vector<T>;
+
+template <typename T, std::size_t N>
+using arr = std::array<T, N>;
+
+template <typename T, typename C = std::less<T>>
+using pq = std::priority_queue<T, std::vector<T>, C>;
+
+template <typename T, typename C = std::less<T>>
+using mset = std::multiset<T, C>;
+
+template <typename T, typename H = std::hash<T>>
+using uset = std::unordered_set<T, H>;
+
+template <typename K, typename V, typename H = std::hash<K>>
+using umap = std::unordered_map<K, V, H>;
+
+template <typename T>
+using cvrefless_t = std::remove_cv_t<std::remove_reference_t<T>>;
+
+template <typename T, typename U, typename... Ts>
+[[nodiscard]] constexpr auto min(T&& x, U&& y, Ts&&... xs) {
+  using R = std::common_type_t<cvrefless_t<T>, cvrefless_t<U>, cvrefless_t<Ts>...>;
+  const std::initializer_list<R> values{
+      static_cast<R>(std::forward<T>(x)), static_cast<R>(std::forward<U>(y)),
+      static_cast<R>(std::forward<Ts>(xs))...};
+  return std::min(values);
+}
+
+template <typename T, typename U, typename... Ts>
+[[nodiscard]] constexpr auto max(T&& x, U&& y, Ts&&... xs) {
+  using R = std::common_type_t<cvrefless_t<T>, cvrefless_t<U>, cvrefless_t<Ts>...>;
+  const std::initializer_list<R> values{
+      static_cast<R>(std::forward<T>(x)), static_cast<R>(std::forward<U>(y)),
+      static_cast<R>(std::forward<Ts>(xs))...};
+  return std::max(values);
+}
+
+using namespace std;
+
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+using f64 = double;
+using f128 = long double;
+
+template <typename T>
+constexpr T MIN = std::numeric_limits<T>::lowest();
+
+template <typename T>
+constexpr T MAX = std::numeric_limits<T>::max();
+
+void YES() { std::cout << "YES\n"; }
+void Yes() { std::cout << "Yes\n"; }
+void yes() { std::cout << "yes\n"; }
+void NO() { std::cout << "NO\n"; }
+void No() { std::cout << "No\n"; }
+void no() { std::cout << "no\n"; }
+
+#if defined(__cpp_lib_print) && __cpp_lib_print >= 202207L
+#define pr(...) std::print(__VA_ARGS__)
+#define prln(...) std::println(__VA_ARGS__)
+#endif
+// }}}
+
+#define FOR(a, b, c) for (int a = b; a < c; ++a)
+
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
-#pragma GCC optimize("O2,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
-using namespace std;
 using namespace __gnu_pbds;
 
 // https://mirror.codeforces.com/blog/entry/124683
@@ -16,7 +88,7 @@ using u64 = std::uint64_t;
 static const u64 FIXED_RANDOM =
     std::chrono::steady_clock::now().time_since_epoch().count();
 
-#if USE_AES
+#if defined(USE_AES) && USE_AES
 std::mt19937 rd(FIXED_RANDOM);
 const __m128i KEY1{(i64)rd(), (i64)rd()};
 const __m128i KEY2{(i64)rd(), (i64)rd()};
@@ -34,7 +106,7 @@ struct custom_hash<T,
                    typename std::enable_if<std::is_integral<T>::value>::type> {
   u64 operator()(T _x) const {
     u64 x = _x;
-#if USE_AES
+#if defined(USE_AES) && USE_AES
     __m128i m{i64(u64(x) * 0xbf58476d1ce4e5b9u64), (i64)FIXED_RANDOM};
     __m128i y = _mm_aesenc_si128(m, KEY1);
     __m128i z = _mm_aesenc_si128(y, KEY2);
@@ -101,56 +173,6 @@ using rbtree = tree<Key, Value, std::less<Key>, rb_tree_tag,
                     tree_order_statistics_node_update>;
 #endif
 
-#define ff first
-#define ss second
-#define eb emplace_back
-#define ll long long
-#define vec vector
-
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (r).rbegin(), (x).rend()
-#define sz(x) static_cast<int>((x).size())
-#define FOR(a, b, c) for (int a = b; a < c; ++a)
-
-#ifdef LOCAL
-#define dbg(x) cout << __LINE__ << ": " << #x << "=<" << (x) << ">\n";
-#else
-#define dbg(x)
-#endif
-
-template <typename... Args> void print(string const &str, Args &&...args) {
-  cout << vformat(str, make_format_args(forward<Args>(args)...));
-}
-
-template <typename T> void print(T const &t) { cout << t; }
-
-template <ranges::range T> void print(T const &t) {
-  if constexpr (is_same_v<T, string> || is_convertible_v<T, char const *>) {
-    cout << t << '\n';
-  } else {
-    for (const auto &e : t) {
-      cout << e << ' ';
-    }
-    cout << '\n';
-  }
-}
-
-template <typename... Args> void println(string const &str, Args &&...args) {
-  print(str, forward<Args>(args)...);
-  cout << '\n';
-}
-
-template <typename T> void println(T const &t) { print("{}\n", t); }
-
-template <ranges::range T> void println(T const &t) {
-  print(t);
-  cout << '\n';
-}
-
-void println() { cout << '\n'; }
-
-template <typename T> T max() { return numeric_limits<T>::max(); }
-
 template <typename T> T min() { return numeric_limits<T>::min(); }
 
 constexpr static int MOD = 1e9 + 7;
@@ -167,7 +189,7 @@ void solve() {
     if (n == 0)
       return d == 0 ? 1 : 0;
 
-    ll total = 0;
+    i64 total = 0;
 
     FOR(i, 1, min(n + 1, k + 1)) {
       total = (total + self(self, n - i, i < d ? d : 0)) % MOD;
@@ -176,18 +198,25 @@ void solve() {
     return memo[{n, d}] = total;
   };
 
-  print(dp(dp, n, d));
+  print("{}", dp(dp, n, d));
 }
 
-int main() {
-  cin.tie(nullptr)->sync_with_stdio(false);
-
-  int t = 1;
-  // cin >> t;
-
-  while (t--) {
+int main() { // {{{
+  std::cin.exceptions(std::cin.failbit);
+#ifdef LOCAL
+  std::cerr.rdbuf(std::cout.rdbuf());
+  std::cout.setf(std::ios::unitbuf);
+  std::cerr.setf(std::ios::unitbuf);
+#else
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+#endif
+  u32 tc = 1;
+  for (u32 t = 0; t < tc; ++t) {
     solve();
   }
-
   return 0;
 }
+// }}}
+
+// vim: foldmethod=marker foldlevel=0
