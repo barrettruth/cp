f78294c7b0a7112c9300fe57ee3132f7ef757d0d
Barrett Ruth <br@barrettruth.com>
2026-08-31 09:47:46 +1000

    refactor(codeforces): standardize contest 918

--- codeforces/918/d.cc
+++ codeforces/918/d.cc
@@ -1,9 +1,48 @@
-#include <bits/stdc++.h>  // {{{
+#include <bits/stdc++.h> // {{{
 
-// https://codeforces.com/blog/entry/96344
+#if __cplusplus >= 202002L && defined(__cpp_lib_ranges)
+namespace rv = std::views;
+namespace rs = std::ranges;
+#endif
 
-#pragma GCC optimize("O2,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+template <typename T>
+using vec = std::vector<T>;
+
+template <typename T, std::size_t N>
+using arr = std::array<T, N>;
+
+template <typename T, typename C = std::less<T>>
+using pq = std::priority_queue<T, std::vector<T>, C>;
+
+template <typename T, typename C = std::less<T>>
+using mset = std::multiset<T, C>;
+
+template <typename T, typename H = std::hash<T>>
+using uset = std::unordered_set<T, H>;
+
+template <typename K, typename V, typename H = std::hash<K>>
+using umap = std::unordered_map<K, V, H>;
+
+template <typename T>
+using cvrefless_t = std::remove_cv_t<std::remove_reference_t<T>>;
+
+template <typename T, typename U, typename... Ts>
+[[nodiscard]] constexpr auto min(T&& x, U&& y, Ts&&... xs) {
+  using R = std::common_type_t<cvrefless_t<T>, cvrefless_t<U>, cvrefless_t<Ts>...>;
+  const std::initializer_list<R> values{
+      static_cast<R>(std::forward<T>(x)), static_cast<R>(std::forward<U>(y)),
+      static_cast<R>(std::forward<Ts>(xs))...};
+  return std::min(values);
+}
+
+template <typename T, typename U, typename... Ts>
+[[nodiscard]] constexpr auto max(T&& x, U&& y, Ts&&... xs) {
+  using R = std::common_type_t<cvrefless_t<T>, cvrefless_t<U>, cvrefless_t<Ts>...>;
+  const std::initializer_list<R> values{
+      static_cast<R>(std::forward<T>(x)), static_cast<R>(std::forward<U>(y)),
+      static_cast<R>(std::forward<Ts>(xs))...};
+  return std::max(values);
+}
 
 using namespace std;
 
@@ -14,48 +53,26 @@ using u64 = uint64_t;
 using f64 = double;
 using f128 = long double;
 
-#if __cplusplus >= 202002L
 template <typename T>
-constexpr T MIN = std::numeric_limits<T>::min();
+constexpr T MIN = std::numeric_limits<T>::lowest();
 
 template <typename T>
 constexpr T MAX = std::numeric_limits<T>::max();
 
-template <typename T>
-[[nodiscard]] static T sc(auto&& x) {
-  return static_cast<T>(x);
-}
+void YES() { std::cout << "YES\n"; }
+void Yes() { std::cout << "Yes\n"; }
+void yes() { std::cout << "yes\n"; }
+void NO() { std::cout << "NO\n"; }
+void No() { std::cout << "No\n"; }
+void no() { std::cout << "no\n"; }
 
-template <typename T>
-[[nodiscard]] static T sz(auto&& x) {
-  return static_cast<T>(x.size());
-}
+#if defined(__cpp_lib_print) && __cpp_lib_print >= 202207L
+#define pr(...) std::print(__VA_ARGS__)
+#define prln(...) std::println(__VA_ARGS__)
 #endif
-
-static void NO() {
-  std::cout << "NO\n";
-}
-
-static void YES() {
-  std::cout << "YES\n";
-}
-
-template <typename T>
-using vec = std::vector<T>;
+// }}}
 
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define ff first
-#define ss second
-
-#ifdef LOCAL
-#define db(...) std::print(__VA_ARGS__)
-#define dbln(...) std::println(__VA_ARGS__)
-#else
-#define db(...)
-#define dbln(...)
-#endif
-//  }}}
 
 void solve() {
   u32 n;
@@ -85,24 +102,23 @@ void solve() {
   println("{}", ans);
 }
 
-int main() {  // {{{
+int main() { // {{{
   std::cin.exceptions(std::cin.failbit);
-
 #ifdef LOCAL
   std::cerr.rdbuf(std::cout.rdbuf());
   std::cout.setf(std::ios::unitbuf);
   std::cerr.setf(std::ios::unitbuf);
 #else
-  std::cin.tie(nullptr)->sync_with_stdio(false);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 #endif
-
   u32 tc = 1;
   std::cin >> tc;
-
   for (u32 t = 0; t < tc; ++t) {
     solve();
   }
-
   return 0;
 }
 // }}}
+
+// vim: foldmethod=marker foldlevel=0
