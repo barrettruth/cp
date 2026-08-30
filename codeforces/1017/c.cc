73b5305dbdf5aab1353d110d7012585291e8c323
Barrett Ruth <br@barrettruth.com>
2026-08-31 09:47:49 +1000

    refactor(codeforces): standardize contest 1017

--- codeforces/1017/c.cc
+++ codeforces/1017/c.cc
@@ -1,9 +1,48 @@
-#include <bits/stdc++.h>  // {{{
+#include <bits/stdc++.h> // {{{
 
-// https://codeforces.com/blog/entry/96344
+#if __cplusplus >= 202002L && defined(__cpp_lib_ranges)
+namespace rv = std::views;
+namespace rs = std::ranges;
+#endif
 
-#pragma GCC optimize("O2,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+template <typename T>
+using vec = std::vector<T>;
+
+template <typename T, std::size_t N>
+using arr = std::array<T, N>;
+
+template <typename T, typename C = std::less<T>>
+using pq = std::priority_queue<T, std::vector<T>, C>;
+
+template <typename T, typename C = std::less<T>>
+using mset = std::multiset<T, C>;
+
+template <typename T, typename H = std::hash<T>>
+using uset = std::unordered_set<T, H>;
+
+template <typename K, typename V, typename H = std::hash<K>>
+using umap = std::unordered_map<K, V, H>;
+
+template <typename T>
+using cvrefless_t = std::remove_cv_t<std::remove_reference_t<T>>;
+
+template <typename T, typename U, typename... Ts>
+[[nodiscard]] constexpr auto min(T&& x, U&& y, Ts&&... xs) {
+  using R = std::common_type_t<cvrefless_t<T>, cvrefless_t<U>, cvrefless_t<Ts>...>;
+  const std::initializer_list<R> values{
+      static_cast<R>(std::forward<T>(x)), static_cast<R>(std::forward<U>(y)),
+      static_cast<R>(std::forward<Ts>(xs))...};
+  return std::min(values);
+}
+
+template <typename T, typename U, typename... Ts>
+[[nodiscard]] constexpr auto max(T&& x, U&& y, Ts&&... xs) {
+  using R = std::common_type_t<cvrefless_t<T>, cvrefless_t<U>, cvrefless_t<Ts>...>;
+  const std::initializer_list<R> values{
+      static_cast<R>(std::forward<T>(x)), static_cast<R>(std::forward<U>(y)),
+      static_cast<R>(std::forward<Ts>(xs))...};
+  return std::max(values);
+}
 
 using namespace std;
 
@@ -13,7 +52,25 @@ using i64 = int64_t;
 using u64 = uint64_t;
 using f64 = double;
 using f128 = long double;
-//  }}}
+
+template <typename T>
+constexpr T MIN = std::numeric_limits<T>::lowest();
+
+template <typename T>
+constexpr T MAX = std::numeric_limits<T>::max();
+
+void YES() { std::cout << "YES\n"; }
+void Yes() { std::cout << "Yes\n"; }
+void yes() { std::cout << "yes\n"; }
+void NO() { std::cout << "NO\n"; }
+void No() { std::cout << "No\n"; }
+void no() { std::cout << "no\n"; }
+
+#if defined(__cpp_lib_print) && __cpp_lib_print >= 202207L
+#define pr(...) std::print(__VA_ARGS__)
+#define prln(...) std::println(__VA_ARGS__)
+#endif
+// }}}
 
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -28,7 +85,7 @@ using u64 = std::uint64_t;
 static const u64 FIXED_RANDOM =
     std::chrono::steady_clock::now().time_since_epoch().count();
 
-#if USE_AES
+#if defined(USE_AES) && USE_AES
 std::mt19937 rd(FIXED_RANDOM);
 const __m128i KEY1{(i64)rd(), (i64)rd()};
 const __m128i KEY2{(i64)rd(), (i64)rd()};
@@ -48,7 +105,7 @@ struct custom_hash<T,
                    typename std::enable_if<std::is_integral<T>::value>::type> {
   u64 operator()(T _x) const {
     u64 x = _x;
-#if USE_AES
+#if defined(USE_AES) && USE_AES
     __m128i m{i64(u64(x) * 0xbf58476d1ce4e5b9u64), (i64)FIXED_RANDOM};
     __m128i y = _mm_aesenc_si128(m, KEY1);
     __m128i z = _mm_aesenc_si128(y, KEY2);
@@ -158,17 +215,23 @@ void solve() {
   }
 }
 
-int main() {  // {{{
-  cin.tie(nullptr)->sync_with_stdio(false);
-  cin.exceptions(cin.failbit);
-
-  int tc = 1;
-  cin >> tc;
-
-  for (int t = 0; t < tc; ++t) {
+int main() { // {{{
+  std::cin.exceptions(std::cin.failbit);
+#ifdef LOCAL
+  std::cerr.rdbuf(std::cout.rdbuf());
+  std::cout.setf(std::ios::unitbuf);
+  std::cerr.setf(std::ios::unitbuf);
+#else
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+#endif
+  u32 tc = 1;
+  std::cin >> tc;
+  for (u32 t = 0; t < tc; ++t) {
     solve();
   }
-
   return 0;
 }
 // }}}
+
+// vim: foldmethod=marker foldlevel=0
